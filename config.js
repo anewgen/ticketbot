48ba78221839d5b9b0707ea308b5a27321642d32
Voyager <cobraboy528@gmail.com>
2025-01-24 19:47:01 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    modmail: {
+        ownerID: '1213207862034174015', // ← Insert the Server Owner ID.
+        supportId: '1331632127430885376', // ← Insert General Support Category ID.
+        premiumId: '1331632127430885376', // ← Insert Premium Support Category ID.
+        whitelist: '1331632100490608711', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1331632100490608711', // ← Insert Staff Role to be pinged upon ticket creation.
+    },
+    logs: {
+        logschannel: '1322824348540731422' // ← Insert Ticket Logs Channel ID.
+    }
+};
